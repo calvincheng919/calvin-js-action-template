d7c5f5773a4b0eb329b2a8b53e1e15d6e97d4548
calvincheng919 <calvin@4mile.io>
2022-09-19 13:02:41 -0700

    cwd

--- index.js
+++ index.js
@@ -7,20 +7,20 @@ const wait = require('./wait')
 // most @actions toolkit packages have async methods
 async function run() {
 
-let workingDirectory = core.getInput("working-directory", { required: false })
-let cwd = workingDirectory ? resolve(workingDirectory) : process.cwd()
-const CWD = cwd + sep
-console.log(cwd)
-
   try {
+    let workingDirectory = core.getInput("working-directory", { required: false })
+    let cwd = workingDirectory ? resolve(workingDirectory) : process.cwd()
+    const CWD = cwd + sep
+
     const ms = core.getInput('milliseconds');
     core.info(`Waiting ${ms} milliseconds ...`);
+    core.info(`working directory is ${CWD} ...`);
 
     core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
     await wait(parseInt(ms));
     core.info((new Date()).toTimeString());
 
-    await exec.exec('touch myfile.js')
+    // await exec.exec('touch myfile.js')
 
     core.setOutput('time', new Date().toTimeString());
   } catch (error) {
