58c599d5d1f9b1afbc8b1b411e37904ce7111970
calvincheng919 <calvin@4mile.io>
2022-09-20 16:33:57 -0700

    v1.8.2

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ const process = require('process')
 const ajv = new Ajv()
 process.chdir('../')
 const cwd = process.cwd();
-const marketplaceRaw = fs.readFileSync(\`\${cwd}/main/marketplace.json\`, 'utf8');
+const marketplaceRaw = fs.readFileSync(\`\${cwd}/marketplace.json\`, 'utf8');
 
 try {
     var marketplace = JSON.parse(marketplaceRaw)
@@ -83,23 +83,23 @@ let schema = {
   describe('Marketplace Automation Tests', ()=> {
 
     test('License File Exists', ()=> {
-        const fileExists = fs.existsSync(\`\${cwd}/main/LICENSE\`);
+        const fileExists = fs.existsSync(\`\${cwd}/LICENSE\`);
         expect(fileExists).toBe(true);
     })
     test('READEME File Exists', ()=> {
-        const fileExists = fs.existsSync(\`\${cwd}/main/readme.md\`);
+        const fileExists = fs.existsSync(\`\${cwd}/readme.md\`);
         expect(fileExists).toBe(true);
     })
     test('Marketplace JSON Exists', ()=> {
-        const fileExists = fs.existsSync(\`\${cwd}/main/marketplace.json\`);
+        const fileExists = fs.existsSync(\`\${cwd}/marketplace.json\`);
         expect(fileExists).toBe(true);
     })
     test('Manifest File Exists', ()=> {
-        const fileExists = fs.existsSync(\`\${cwd}/main/manifest.lkml\`);
+        const fileExists = fs.existsSync(\`\${cwd}/manifest.lkml\`);
         expect(fileExists).toBe(true);
     })
     test('At Least One Dashboard File Exists', ()=> {
-        const files = fs.readdirSync(\`\${cwd}/main/dashboards/\`);
+        const files = fs.readdirSync(\`\${cwd}/dashboards/\`);
         dashboardFiles = files.filter( item => {
             return item.includes('dashboard.lookml')
         })
