be8b923db794074c507e355e31b993926a76987b
calvincheng919 <calvin@4mile.io>
2022-09-22 10:04:11 -0700

    v3.0

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const { sep, resolve} = require("path")
 const fs = require('fs');
 const core = require('@actions/core');
 const exec = require('@actions/exec');
-const package = require('./package_.json')
-const testCode = require('./marketplace_tests.js').getTests()
+// const package = require('./package_.json')
+// const testCode = require('./marketplace_tests.js').getTests()
 require('dotenv').config()
 
 async function run() {
-  readWritePackage();
-  readWriteTestFile();
+  // readWritePackage();
+  // readWriteTestFile();
   try {
     let workingDirectory = core.getInput("working-directory", { required: false })
     let cwd = workingDirectory ? resolve(workingDirectory) : process.cwd()
@@ -20,19 +20,19 @@ async function run() {
   }
 }
 
-function readWritePackage() {
-  if (process.env.DEV) {
-    package.scripts.prepare = 'ncc build index.js -o dist --source-map --license licenses.txt'
-  }
-  fs.writeFile("package.json", JSON.stringify(package), (err) => {
-    if (err) console.log(err);
-  });
-}
+// function readWritePackage() {
+//   if (process.env.DEV) {
+//     package.scripts.prepare = 'ncc build index.js -o dist --source-map --license licenses.txt'
+//   }
+//   fs.writeFile("package.json", JSON.stringify(package), (err) => {
+//     if (err) console.log(err);
+//   });
+// }
 
-function readWriteTestFile() {
-  fs.writeFile("marketplace.spec.js", testCode, (err) => {
-    if (err) console.log(err);
-  });
-}
+// function readWriteTestFile() {
+//   fs.writeFile("marketplace.spec.js", testCode, (err) => {
+//     if (err) console.log(err);
+//   });
+// }
 
 run();
